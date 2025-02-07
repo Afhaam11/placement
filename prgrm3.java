39add2855901e96d492c9b40f2bd103288d7d8cf
MUHAMMED AFHAAM <afhaam1111@gmail.com>
2025-02-07 16:17:45 +0530

    Add files via upload

---
+++ prgrm3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class prgrm3 {
+    
+
+    public static void main(String args[]){
+        int[] arr = {12,4,7,4,8,7};
+
+        HashSet<Integer> elements = new HashSet<>();
+
+        for(int i=0;i<arr.length;i++){
+            if(elements.contains(arr[i]))
+            {
+                System.out.println("Duplicate is: "+arr[i]);
+                break;
+            }
+            elements.add(arr[i]);
+        }
+    }
+}
+
